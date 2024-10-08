46ad98b58f4959c16f84702b4ef5721098707920
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-10-08 03:58:19 -0500

    [fix] 404 page

--- src/pages/Notfound/NotFound.tsx
+++ src/pages/Notfound/NotFound.tsx
@@ -1,8 +1,14 @@
+// NotFound.js
+// import { Link } from 'react-router-dom';
+import './NotFound.css'; // Optional: for styling
+
 const NotFound = () => {
   return (
-    <>
-      <h1>404 Page Not Found</h1>
-    </>
+    <div className="flex flex-col gap-16 items-center justify-center h-screen bg-black">
+      <div className="text-[100px] text-danger font-bold">404</div>
+      <div className="text-[20px] text-white font-bold">Page Not Found</div>
+    </div>
   );
 };
+
 export default NotFound;
