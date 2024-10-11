f85b1c115da041d118b8e3e9e614121d38d39f4c
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-10-10 23:31:26 -0500

    [fix] top search bar

--- src/components/TopSearchBar/index.tsx
+++ src/components/TopSearchBar/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { imgGear } from '../../images/icon';
-import { Search } from 'lucide-react';
 import Toolbar from '../Toolbar';
 
 interface TopSearchBarProps {
