ab4dff63d8752da3fe6a60fa80d25ed42a3155e5
acmicpc0614 <pleasebugmenot.dev@gmail.com>
2024-10-15 16:20:23 -0500

    [feat] operation page

--- src/pages/Configuration/OperationsTypes.tsx
+++ src/pages/Configuration/OperationsTypes.tsx
@@ -1,5 +1,21 @@
+import OperationTable from '../../components/OperationTable';
+import TopSearchBar from '../../components/TopSearchBar';
+
 const OperationsTypes = () => {
-  return <>OperationsTypes</>;
+  const buttons = [{ label: 'New', path: 'new' }];
+  return (
+    <>
+      <div className="w-full h-full flex flex-col gap-5">
+        <TopSearchBar
+          title="Operations Types"
+          btns={buttons}
+          pagination={true}
+          tools={false}
+        />
+        <OperationTable />
+      </div>
+    </>
+  );
 };
 
 export default OperationsTypes;
--- src/pages/Configuration/ProductCategories.tsx
+++ src/pages/Configuration/ProductCategories.tsx
@@ -1,5 +1,21 @@
+import OperationTable from '../../components/OperationTable';
+import TopSearchBar from '../../components/TopSearchBar';
+
 const ProductCategories = () => {
-  return <>ProductCategories</>;
+  const buttons = [{ label: 'New', path: 'new' }];
+  return (
+    <>
+      <div className="w-full h-full flex flex-col gap-5">
+        <TopSearchBar
+          title="Product Category"
+          btns={buttons}
+          pagination={true}
+          tools={false}
+        />
+        <OperationTable />
+      </div>
+    </>
+  );
 };
 
 export default ProductCategories;
